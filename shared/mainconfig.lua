d65868a228bbed2404e3c270a457d81ef802f982
blakeayye <bcoram04@gmail.com>
2023-03-22 18:17:17 -0400

    w

---
+++ shared/mainconfig.lua
@@ -0,0 +1,142 @@
+Config = {} 
+Config.img = "qb-inventory/html/images/"
+Config.Debug = true
+
+
+Config.TraderPeds = {
+    [1] = { -- recyclablematerial
+        coords = vector4(-1101.57, 2721.68, 18.8, 2.3),
+        model = "s_m_o_busker_01",
+        scene = "WORLD_HUMAN_AA_COFFEE",
+        icon = 'fa-solid fa-bolt',
+        label = 'Talk to Namir',
+        product = {
+        item = "recyclablematerial",
+        amount = 100,
+        removeitem = "matcoupon",
+        removeitemamount = 1,
+        },
+       
+    },
+    [2] = { -- aluminum
+        coords = vector4(-1861.8, -317.13, 49.15, 341.3),
+        model = "s_m_m_lathandy_01",
+        scene = "WORLD_HUMAN_AA_COFFEE",
+        icon = 'fa-solid fa-bolt',
+        label = 'Talk to Nabir',
+        product = {
+            item = "refined_aluminum",
+            amount = 4,
+            removeitem = "aluminum",
+            removeitemamount = 20,
+        },
+      
+        
+    },
+    [3] = { -- iron
+        coords = vector4(852.74, 2133.78, 52.28, 127.66),
+        model = "s_m_y_construct_01",
+        scene = "world_human_clipboard",
+        icon = 'fa-solid fa-bolt',
+        label = 'Talk to Rabira',
+        product = {
+            item = "refined_iron",
+            amount = 4,
+            removeitem = "iron",
+            removeitemamount = 20,
+        },
+      
+        
+    },
+    [4] = { -- steel
+        coords = vector4(-349.8787, -1569.8713, 25.2229, 297.9694),
+        model = "s_m_y_dockwork_01",
+        scene = "WORLD_HUMAN_AA_SMOKE",
+        icon = 'fa-solid fa-bolt',
+        label = 'Talk to Mabir',
+        product = {
+            item = "refined_steel",
+            amount = 3,
+            removeitem = "steel",
+            removeitemamount = 20,
+        },
+      
+        
+    },
+    [5] = { -- plastic
+        coords = vector4(589.6116, -3276.8467, 6.0696, 351.4336),
+        model = "s_m_m_autoshop_01",
+        scene = "WORLD_HUMAN_DRUG_DEALER",
+        icon = 'fa-solid fa-bolt',
+        label = 'Talk to Labir',
+        product = {
+            item = "refined_plastic",
+            amount = 4,
+            removeitem = "plastic",
+            removeitemamount = 20,
+        },
+      
+        
+    },
+    [6] = { -- copper
+        coords = vector4(2928.34, 4272.49, 50.42, 349.57),
+        model = "s_m_y_garbage",
+        scene = "WORLD_HUMAN_AA_SMOKE",
+        icon = 'fa-solid fa-bolt',
+        label = 'Talk to Zabir',
+        product = {
+            item = "refined_copper",
+            amount = 4,
+            removeitem = "copper",
+            removeitemamount = 20,
+        },
+      
+        
+    },
+    [7] = { -- glass
+        coords = vector4(59.43, 6311.96, 31.38, 158.32),
+        model = "s_m_m_pilot_01",
+        scene = "WORLD_HUMAN_DRUG_DEALER",
+        icon = 'fa-solid fa-bolt',
+        label = 'Talk to Vabir',
+        product = {
+            item = "refined_glass",
+            amount = 4,
+            removeitem = "glass",
+            removeitemamount = 20,
+        },
+      
+        
+    }, -- rubber
+    [8] = { 
+        coords = vector4(-1090.96, -2408.98, 13.95, 331.18),
+        model = "csb_ramp_mex",
+        scene = "WORLD_HUMAN_AA_SMOKE",
+        icon = 'fa-solid fa-bolt',
+        label = 'Talk to Babir',
+        product = {
+            item = "refined_rubber",
+            amount = 4,
+            removeitem = "rubber",
+            removeitemamount = 20,
+        },
+      
+        
+    },
+   --[[ [9] = { -- aluminum
+        coords = vector4(-35.35, -2689.88, 6.0, 44.67),
+        model = "s_m_m_gardener_01",
+        scene = "WORLD_HUMAN_DRUG_DEALER",
+        icon = 'fa-solid fa-bolt',
+        label = 'Talk to Fabir',
+        product = {
+            item = "refined_aluminum",
+            amount = 4,
+            removeitem = "aluminum",
+            removeitemamount = 20,
+        },
+      
+        
+    },]]
+}
+
